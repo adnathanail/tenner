77b375e6cd0249c87731d98445409a47b5294040
Alexander Nathanail <adnathanail@hotmail.co.uk>
2017-02-24 15:47:04 +0000

    Created database V1

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.utils import timezone
-
+from datetime import datetime
 
 class Item(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50, default='')
+    price = models.DecimalField(max_digits=4, decimal_places=2, default=0)
 
     def __str__(self):
-        return self.name
+        return self.name + ' - £' + str(self.price)
+
+class Order(models.Model):
+    orderer = models.ForeignKey('auth.User', null=True)
+    orderDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.orderer) #str(self.orderDate)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=True)
+    item = models.ForeignKey(Item, null=True)
+    quantity = models.IntegerField( default=0)
+
+    def __str__(self):
+        return str(self.quantity) + "x " + self.item.name
