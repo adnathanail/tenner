d2a7193d81b60153622ecf078a2682ed852e4b73
Alexander Nathanail <adnathanail@hotmail.co.uk>
2017-02-26 21:46:39 +0000

    Created login and registration system

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    url(r'^$', views.order_list, name='order_list'),
+    url(r'^$', views.home, name='home'),
+    url(r'^orderList/$', views.orderList, name='orderList'),
+    url(r'^makeOrder/$', views.makeOrder, name='makeOrder'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^register/$', views.register_page, name='register'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from .models import Order, OrderItem
+from django.contrib.auth.models import User
+from .forms import *
+from django.http import HttpResponseRedirect
 
 # Create your views here.
-def order_list(request):
+def orderList(request):
     orders = Order.objects.filter(completed=False)
     orderItems = OrderItem.objects.all()
     return render(request, 'orders/order_list.html', {'orders':orders,'orderItems':orderItems})
+
+def home(request):
+    return render(request, 'orders/home.html', {})
+
+def makeOrder(request):
+    return render(request, 'orders/make_order.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
+            return HttpResponseRedirect('/')
+    form = RegistrationForm()
+    return render(request, 'registration/register.html',{'form': form})
